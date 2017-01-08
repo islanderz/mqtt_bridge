e859e829cae3d933360b5a528e9396a53b93f11d
Harshit Sureka <harshit.sureka@gmail.com>
2017-01-09 02:38:31 +0530

    MAJOR COMMIT that adds 2 launch files 1) Bridge.launch launches the bridge along with the mqttPingRequestor 2) pingResponder.launch launches the pingResponder thread
    Currently the localhost is hard coded as the mqtt broker, this will be changed to a parameter later.
    
    in order to run now, roslaunch mqtt_bridge bridge.launch on 1st machine and roslaunch mqtt_bridge pingResponder.launch on second machine.

---
+++ src/mqtt_bridge/mqttPingRequester.py
@@ -0,0 +1,55 @@
+import paho.mqtt.client as mqtt
+import datetime
+
+
+def on_connect(mosq, obj, rc):
+    print("rc: " + str(rc))
+
+def on_message(mosq, obj, msg):
+#    print(msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    currentTime = datetime.datetime.now();
+    timeInMessage = datetime.datetime.strptime(str(msg.payload),"%Y-%m-%d %H:%M:%S.%f");
+#   print("Recd at " + str(datetime.datetime.now()));
+    print("delay is: " + str((currentTime - timeInMessage).total_seconds() * 1000));
+
+def on_publish(mosq, obj, mid):
+    print("mid: " + str(mid))
+
+def on_subscribe(mosq, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+def on_log(mosq, obj, level, string):
+    print(string)
+
+
+def pingRequestFunction():   
+  mqttc = mqtt.Client()
+# Assign event callbacks
+  mqttc.on_message = on_message
+  mqttc.on_connect = on_connect
+  mqttc.on_publish = on_publish
+  mqttc.on_subscribe = on_subscribe
+# Connect
+  mqttc.connect("iot.eclipse.org", 1883,60)
+#  mqttc.connect("localhost", 1883,60)
+
+# Start subscribe, with QoS level 0
+  mqttc.subscribe("mqtt/pings/response", 0)
+
+# Publish a message
+#mqttc.publish("hello/world", "my message")
+
+# Continue the network loop, exit when an error occurs
+  rc = 0
+  lastPublishedTime = datetime.datetime.now();
+  while rc == 0:
+    rc = mqttc.loop()
+    currentTime = datetime.datetime.now();
+    if(currentTime - lastPublishedTime > datetime.timedelta(seconds=1)):
+      lastPublishedTime = datetime.datetime.now();
+      message = str(lastPublishedTime);
+      mqttc.publish("mqtt/pings/request",message)
+#      print("Sent at " + message);
+#time.sleep(1)
+  print("rc: " + str(rc))
+  print("ping thread disconnected")
